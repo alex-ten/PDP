81992444990cbc99e1eb0fc9aceb6e1c0028f927
alexten <tenalexander1991@gmail.com>
2016-08-12 18:49:07 +0600

    Add another version of XOR program to use the new class-based API

---
+++ FFBP/xor_v3.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+import FFBP.utilities.evaluation_functions as evalf
+import FFBP.utilities.activation_functions as actf
+import FFBP.utilities.error_functions as errf
+import FFBP.utilities.train as train
+from FFBP.classes.DataSet import load_data
+from FFBP.classes.Network import Network
+from FFBP.classes.Layer import Layer
+from FFBP.utilities.model import model
+
+DS = load_data('ex_XOR/f_XOR.txt')
+
+image = tf.placeholder(tf.float32, shape=[None,2], name='input') # image / img_pat
+label = tf.placeholder(tf.float32, shape=[None,1], name='target') # label / lbl_pat
+
+hidden1 = Layer(input_tensor = image,
+                size = 2,
+                wrange = [-1,1],
+                act = actf.sigmoid,
+                layer_name = 'hidden1',
+                seed = 9)
+
+output =  Layer(input_tensor = hidden1.activations,
+                size = 1,
+                wrange = [-1,1],
+                act = actf.sigmoid,
+                layer_name = 'output',
+                seed = 2)
+
+xor = model([image], [hidden1, output], label)
+
+mynet = Network(xor)
+mynet.dataset = DS
+mynet.init_weights()
+mynet.restore('ex_XOR/xor_params.ckpt')
+
+batch_size = 4
+lrate = 0.5
+mrate = 0.9
+num_epochs = 300
+
+for s in range(3):
+    mynet.train(num_epochs = None,
+                learning_rate = lrate,
+                momentum = mrate,
+                error = errf.squared_error,
+                batch_size = batch_size,
+                checkpoint = 300,
+                permute = False)
+    mynet.test(batch_size=batch_size)
+mynet.print_logdir()
+mynet.off()
