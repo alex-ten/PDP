2cb276b77624e1811beec77fc5ecc33c857fd087
alexten <tenalexander1991@gmail.com>
2016-08-15 04:29:26 +0600

    This is what the previous commit should have been

--- FFBP/utilities/general_slider.py
+++ FFBP/utilities/general_slider.py
@@ -1,42 +1,71 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button, RadioButtons
+from matplotlib.widgets import Slider
+
+matplotlib.style.use('ggplot')
+
+# define a function of x - the slider value
+def myfunc(x):
+    return -(x ** 2) + 16
 
 fig, ax = plt.subplots()  # create a figure and axis (i.e. subplot) instances
 plt.subplots_adjust(left=0.25, bottom=0.25) # adjust subplot position
-t = np.arange(0.0, 1.0, 0.001) # range of 1000 values between 0 and 1
-a0 = 5
-s = a0*np.sin(2*np.pi*t) # define relationships of x and constants to y
-l, = plt.plot(t, s, lw=2, color='red') # draw s as a function of t
-plt.axis([0, 1, -10, 10]) # set max and min values for axes [xmin, xmax, ymin, ymax]
+x = np.arange(0,4,0.01) # range of 400 values between 0 and 4
+y = myfunc(x) # define relationships between x and y
+line, = plt.plot(x, y, lw=2, color='#3DB1FF') # draw s as a function of t
+plt.xlabel('x')
+plt.ylabel('f(x) = -(x^2) + 16')
 
-axcolor = 'lightgoldenrodyellow' # assign the value for axis color to axcolor
-axamp = plt.axes([0.25, 0.15, 0.65, 0.03], axisbg=axcolor) # assign position for axes for AMPLITUDE SLIDER
 
-samp = Slider(axamp, 'Amp', 0.1, 10.0, valinit=a0) # create a slider instance for AMPLITUDE using axamp from above
+# Set initial state of the slider graph
+hor = plt.axhline(y[-1], c='black', lw='0.8', ls=':') # add horizontal line
+ver = plt.axvline(x[-1], c='black', lw='0.8', ls=':') # add vertical line
 
+# place annotation near the last (x,y) point
+ann = ax.annotate('f({}): {}'.format(np.around(x[-1], 2), np.around(y[-1],2)),  # annotation string
+                  # annotation coordinates:
+                  xy=(x[-1], y[-1]),  # position of element to annotate
+                  xycoords='data',  # use xy to define the position in the coordinate system being annotated
+                  xytext=(28, 26),  # position of the annotation
+                  textcoords='offset points',  # specify an offset from the xy coordinates
+                  # text properties
+                  size=12,  # size in points
+                  verticalalignment="center",  # position of text along the vertical axis of the box [aka va]
+                  horizontalalignment="center",  # position of text along the horizontal axis of the box [aka ha]
+                  bbox=dict(boxstyle="round",  # box shape (alt. square)
+                            facecolor="#66B8FF",
+                            edgecolor="none",
+                            alpha=0.5),
+                  arrowprops=dict(arrowstyle="wedge, tail_width=0.3, shrink_factor=1",
+                                  # if arrowstyle key is present FancyArrowPatch prop dict is used
+                                  fc="#66B8FF",
+                                  ec="none",
+                                  alpha=0.3,
+                                  patchA=None,
+                                  patchB=None,
+                                  relpos=(0.5, 0.00),
+                                  )
+                  )
+
+plt.axis([0, 4, 0, 20]) # set max and min values for axes [xmin, xmax, ymin, ymax]
+
+slide1_background_color = '#BDE9FF' # set the slide background color
+slide1_axis = plt.axes([0.25, 0.12, 0.65, 0.03], axisbg=slide1_background_color) # assign position for axes for slider
+
+# create a slider instance for AMPLITUDE using axamp from above
+slide1 = Slider(slide1_axis, 'x', 0.0, 4.0, valinit=x[-1], fc='#588CBF')
 
 def update(val):
-    amp = samp.val # assign value of samp Slider to amp
-    l.set_ydata(amp*np.sin(2*np.pi*t)) # set y_data to l.plot.ydata (i.e. a 2D line)
-    fig.canvas.draw_idle() # draws 2D line
-samp.on_changed(update) # When the slider value is changed, call update with the new slider position
+    _x = slide1.val # assign value of the slider to _x (the dynamic value of x)
+    _y = myfunc(_x) # dynamic value of _y is defined my the same relationship function
+    ann.xy = (_x,_y) # set annotation xy to new values
+    ann.set_text('f({}): {}'.format(np.around(_x, 2), np.around(_y,2))) # change text accordingly
+    # change positions of straight lines
+    hor.set_ydata(_y)
+    ver.set_xdata(_x)
+    fig.canvas.draw_idle() # draw 2D line
 
-resetax = plt.axes([0.8, 0.025, 0.1, 0.04]) # reserve space for Reset button
-button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975') # create a button instance
-
-
-def reset(event):
-    samp.reset()
-button.on_clicked(reset)
-
-rax = plt.axes([0.025, 0.5, 0.15, 0.15], axisbg=axcolor)
-radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)
-
-
-def colorfunc(label):
-    l.set_color(label)
-    fig.canvas.draw_idle()
-radio.on_clicked(colorfunc)
+slide1.on_changed(update) # when the slider value is changed, call update function with the new slider position
 
 plt.show()
