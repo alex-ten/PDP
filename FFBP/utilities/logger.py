39873ab23b94d37bf26d1bfb1ccef8716ae8f8f4
alexten <tenalexander1991@gmail.com>
2016-08-19 01:24:10 +0600

    Add a logger utility containing various convenience functions for data logging

---
+++ FFBP/utilities/logger.py
@@ -0,0 +1,41 @@
+import numpy as np
+import datetime as dt
+import os
+
+def logdir():
+    # Create logdir directory if it doesn't exist
+    logdir_dir = os.getcwd() + '/logdir'
+    try:
+        os.mkdir(logdir_dir)
+        # Name sess directory according to current date-time
+        sess_name = 'Sess_' + dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        dir_path = logdir_dir + '/' + sess_name
+        os.mkdir(dir_path)
+        os.mkdir(dir_path + '/params')
+        os.mkdir(dir_path + '/mpl_data')
+        os.mkdir(dir_path + '/TFEvents')
+    except FileExistsError:
+        sess_name = 'Sess_'+dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        dir_path = logdir_dir + '/' + sess_name
+        os.mkdir(dir_path)
+        os.mkdir(dir_path + '/params')
+        os.mkdir(dir_path + '/mpl_data')
+        os.mkdir(dir_path + '/TFEvents')
+    return dir_path
+
+def unroll(ar, index):
+    if type(ar) is list: ar = ar[0]
+    strip1D = np.insert(ar.flatten(), 0, [index] + list(ar.shape))
+    return np.reshape(strip1D, (1,strip1D.size))
+
+def rollup(strip):
+    pass
+
+def append_snapshot(old, new):
+    for KEY in old.keys():
+        if KEY == 'error':
+            old[KEY] = np.append(old[KEY], new[KEY], axis=0)
+        else:
+            for key in old[KEY].keys():
+                old[KEY][key] = np.append(old[KEY][key], new[KEY][key], axis=0)
+    return old
