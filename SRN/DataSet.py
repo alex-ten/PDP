9927bca3100f851055f66de583603c7f88bd9002
alexten <tenalexander1991@gmail.com>
2016-09-26 05:08:30 +0600

    next_batch returns feed-ready arrays

---
+++ SRN/DataSet.py
@@ -0,0 +1,103 @@
+import numpy as np
+from collections import OrderedDict
+import pickle
+from time import sleep
+
+def make_oh_map(chars):
+    map_ = OrderedDict()
+    num_chars = len(chars)
+    for ind, char in enumerate(sorted(chars)):
+        z = np.zeros(num_chars)
+        z[ind] = 1.0
+        map_[char] = z
+    map_['_length_'] = num_chars
+    return map_
+
+def str2oh(raw, max_length, oh_map, NaN_fill = False):
+    num_cols = oh_map['_length_']
+    num_rows = max_length
+    num_planes = len(raw)
+    sequence_x = np.zeros((num_planes, num_rows, num_cols))
+    sequence_y = np.zeros((num_planes, num_rows, num_cols))
+    if NaN_fill:
+        sequence_x[:] = np.NaN
+        sequence_y[:] = np.NaN
+    for plane_ind, sequence in enumerate(raw):
+        for row_ind, char in enumerate(sequence):
+            sequence_x[plane_ind,row_ind] = oh_map[char]
+        sequence_y[plane_ind,0:-1] = sequence_x[plane_ind,1:]
+
+    return sequence_x, sequence_y
+
+class DataSet(object):
+    def __init__(self, path):
+        with open(path, 'rb') as file:
+            jar = pickle.load(file)
+            self.unique = jar.pop()
+            self.raw = jar
+        self.num_seqs = len(self.raw)
+        self.oh_map = make_oh_map(self.unique)
+        self.max_length = len(max(self.raw, key=len))
+        self.inp_seqs = None
+        self.out_seqs = None
+        self._current_batches = None
+        self._current_batches_size = None
+        self._batches_ind = 0
+        self._batch_ind = 0
+
+
+    def raw2onehot(self, NaN_fill = False):
+        self.inp_seqs, self.out_seqs = str2oh(raw = self.raw,
+                                              max_length = self.max_length,
+                                              oh_map = self.oh_map,
+                                              NaN_fill = NaN_fill)
+
+    def _get_batches(self, num_seqs):
+        assert self.num_seqs % num_seqs == 0, 'Number of batches must divide the total ' \
+                                                 'number of sequences. Got {} % {} = {}'.format(self.max_length,
+                                                                                                num_seqs,
+                                                                                                self.max_length % num_seqs)
+        start = self._batches_ind
+        self._batches_ind += num_seqs
+        if self._batches_ind > self.num_seqs:
+            start = 0
+            self._batches_ind = num_seqs
+        end = self._batches_ind
+        _x, _y = np.squeeze(self.inp_seqs[start:end,:]), np.squeeze(self.out_seqs[start:end,:])
+        zxy_x, zxy_y = np.shape(_x), np.shape(_y)
+
+        _x = np.reshape(_x, (zxy_x[0] * zxy_x[1], zxy_x[2]), order='F')
+        _y = np.reshape(_x, (zxy_y[0] * zxy_y[1], zxy_y[2]), order='F')
+        self._current_batches_size = num_seqs * self.max_length
+        self._current_batches = _x, _y
+
+    def next_batch(self, batch_size):
+        if not self._batch_ind:
+            self._get_batches(batch_size)
+        assert self._current_batches_size % batch_size == 0, 'Batch size must divide the total ' \
+                                                             'number of sequences. Got {} % {} = {}'.format(self.max_length,
+                                                                                                            batch_size,
+                                                                                                            self.max_length % batch_size)
+        start = self._batch_ind
+        self._batch_ind += batch_size
+        if self._batch_ind > batch_size * self.max_length:
+            self._get_batches(batch_size)
+            start = 0
+            self._batch_ind = batch_size
+        end = self._batch_ind
+        xs, ys = self._current_batches
+        return xs[start:end], ys[start:end]
+
+    def all_seqs(self):
+        return (self.inp_seqs, self.out_seqs)
+
+
+def main():
+    data = DataSet('240.pkl')
+    data.raw2onehot()
+    print(data.raw)
+
+    for i in range(8):
+        x, y = data.next_batch(2)
+
+if __name__=='__main__': main()
---
+++ SRN/FSM.py
@@ -0,0 +1,103 @@
+from random import randint
+import pickle
+
+State = type('State', (object,), {})
+Transition = type('Transition', (object,), {})
+
+class IntState(State):
+    def __init__(self, *args, **kwargs):
+        args = list(args)
+        self.i = args.pop(0)
+        super(IntState, self).__init__(*args, **kwargs)
+    def Exec(self):
+        print('state '+str(self.i))
+
+
+class SimpleTransition(Transition):
+    def __init__(self, *args, **kwargs):
+        args = list(args)
+        self.to = args.pop()
+        self.from_ = args.pop()
+        self.sid = kwargs.pop('sid')
+        super(SimpleTransition, self).__init__()
+    def Exec(self):
+        return self.to
+
+
+class FSM():
+    def __init__(self, states, transitions):
+        self.start = 'B'
+        self.end = 'E'
+        self.states = states
+        self.transitions = transitions
+        self.num_states = len(states)
+        self.in_state = states[0]
+        self.in_trans = None
+        self.state_history = []
+        self.trans_history = []
+        self.composition = self.start
+        self.trans_set = list(set([t.sid for t in transitions] + [self.start, self.end]))
+
+    def transition(self, record = False):
+        fork = [x for x in self.transitions if x.from_ == self.in_state]
+        choice = fork[randint(0,1)]
+        self.composition += choice.sid
+        if record:
+            self.state_history.append(self.in_state)
+            self.trans_history.append(choice.sid)
+        self.in_state = choice.Exec()
+
+
+    def utter(self, max_units):
+        self.reset()
+        for step in range(max_units):
+            self.transition()
+            if self.in_state == self.states[-1]: break
+        self.composition += 'E'
+        print(self.composition)
+        return self.composition
+
+    def chatter(self, num_utterances, max_chars):
+        chatter_log = []
+        for i in range(num_utterances):
+            chatter_log.append(self.utter(max_chars))
+        chatter_log.append(self.trans_set)
+        return chatter_log
+
+    def reset(self):
+        self.in_state = self.states[0]
+        self.composition = 'B'
+
+def store_random_chatter(log, path):
+    pickle.dump(log, open(path, 'wb'))
+
+def make_states(l):
+    return [IntState(s) for s in l]
+
+
+def make_transitions(td):
+    return [SimpleTransition(k[0],k[1], sid = v) for k,v in td.items()]
+
+
+def main():
+    l = list(range(0,6))
+    states = make_states(l)
+    td = {(states[0],states[1]): 'T',
+          (states[2],states[2]): 'T',
+          (states[1],states[1]): 'S',
+          (states[3],states[5]): 'S',
+          (states[1],states[3]): 'X',
+          (states[3],states[2]): 'X',
+          (states[2],states[4]): 'V',
+          (states[4],states[5]): 'V',
+          (states[4],states[3]): 'P',
+          (states[0],states[2]): 'P',
+          }
+    transitions = make_transitions(td)
+
+    myFSM = FSM(states, transitions)
+    chatter_log = myFSM.chatter(240,10)
+    store_random_chatter(chatter_log, '240.pkl')
+
+if __name__=='__main__': main()
+
