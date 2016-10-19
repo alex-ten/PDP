a52db0b46d7d3c185f968610e70555b96a09b88f
alexten <tenalexander1991@gmail.com>
2016-10-19 14:37:27 +0100

    Fix padding style
    End of sequence does not appear in the input

--- SRN/FSM/DataSet.py
+++ SRN/FSM/DataSet.py
@@ -1,7 +1,7 @@
 import numpy as np
 from collections import OrderedDict
 import pickle
-from time import sleep
+import random
 
 def make_oh_map(chars):
     map_ = OrderedDict()
@@ -12,23 +12,6 @@ def make_oh_map(chars):
         map_[char] = z
     map_['_length_'] = num_chars
     return map_
-
-
-def str2oh(raw, max_length, oh_map, NaN_fill = False):
-    num_cols = oh_map['_length_']
-    num_rows = max_length
-    num_planes = len(raw)
-    sequence_x = np.zeros((num_planes, num_rows, num_cols))
-    sequence_y = np.zeros((num_planes, num_rows, num_cols))
-    if NaN_fill:
-        sequence_x[:] = np.NaN
-        sequence_y[:] = np.NaN
-    for plane_ind, sequence in enumerate(raw):
-        for row_ind, char in enumerate(sequence):
-            sequence_x[plane_ind,row_ind] = oh_map[char]
-        sequence_y[plane_ind,0:-1] = sequence_x[plane_ind,1:]
-
-    return sequence_x, sequence_y
 
 
 class DataSet(object):
@@ -51,39 +34,35 @@ class DataSet(object):
         self._batch_ind = 0
 
     def raw2onehot(self, NaN_fill = False):
-        self.inp_seqs, self.out_seqs = str2oh(raw = self.raw,
-                                              max_length = self.max_length,
-                                              oh_map = self.oh_map,
-                                              NaN_fill = NaN_fill)
+        num_cols = self.oh_map['_length_']
+        num_rows = self.max_length - 1
+        num_planes = len(self.raw)
+        self.inp_seqs = np.zeros((num_planes, num_rows, num_cols))
+        self.out_seqs = np.zeros((num_planes, num_rows, num_cols))
+        if NaN_fill:
+            self.inp_seqs[:] = np.NaN
+            self.out_seqs[:] = np.NaN
+        for plane_ind, sequence in enumerate(self.raw):
+            l = len(sequence)
+            seq_placeholder = np.zeros((l, num_cols))
+            for row_ind, char in enumerate(sequence):
+                seq_placeholder[row_ind] = self.oh_map[char]
+            self.inp_seqs[plane_ind][:l-1] = seq_placeholder[:-1]
+            self.out_seqs[plane_ind][:l-1] = seq_placeholder[1:]
 
     def raw2inds(self):
-        self.inp_inds = np.zeros(shape = [self.num_seqs, self.max_length])
-        self.out_inds = np.zeros(shape = [self.num_seqs, self.max_length])
-        for ii, string in enumerate(self.raw):
-            for jj, char in enumerate(string):
-                self.inp_inds[ii, jj] = self.unique.index(char)
-                self.out_inds[ii, 0:-1] = self.inp_inds[ii, 1:]
+        self.inp_inds = np.zeros(shape = [self.num_seqs, self.max_length - 1])
+        self.out_inds = np.zeros(shape = [self.num_seqs, self.max_length - 1])
+        for row, sequence in enumerate(self.raw):
+            l = len(sequence)
+            seq_placeholder = np.zeros(l)
+            for i, char in enumerate(sequence):
+                seq_placeholder[i] = self.unique.index(char)
+            self.inp_inds[row][0:l-1] = seq_placeholder[:-1]
+            self.out_inds[row][0:l-1] = seq_placeholder[1:]
 
-    def _get_batches(self, num_seqs):
-        assert self.num_seqs % num_seqs == 0, 'Number of batches must divide the total ' \
-                                                 'number of sequences. Got {} % {} = {}'.format(self.max_length,
-                                                                                                num_seqs,
-                                                                                                self.max_length % num_seqs)
-        start = self._batches_ind
-        self._batches_ind += num_seqs
-        if self._batches_ind > self.num_seqs:
-            start = 0
-            self._batches_ind = num_seqs
-        end = self._batches_ind
-        _x, _y = np.squeeze(self.inp_seqs[start:end,:]), np.squeeze(self.out_seqs[start:end,:])
-        zxy_x, zxy_y = np.shape(_x), np.shape(_y)
 
-        _x = np.reshape(_x, (zxy_x[0] * zxy_x[1], zxy_x[2]), order='F')
-        _y = np.reshape(_x, (zxy_y[0] * zxy_y[1], zxy_y[2]), order='F')
-        self._current_batches_size = num_seqs * self.max_length
-        self._current_batches = _x, _y
-
-    def next_batch(self, batch_size, ind_batch_X = False, ind_batch_Y = False, truncate=True):
+    def next_batch(self, batch_size, ind_batch_X = False, ind_batch_Y = False):
         start = self._batch_ind
         self._batch_ind += batch_size
         if self._batch_ind > self.num_seqs:
@@ -91,31 +70,45 @@ class DataSet(object):
             self._batch_ind = batch_size
         end = self._batch_ind
         _x, _y = self.inp_seqs[start:end,:,:], self.out_seqs[start:end,:,:]
-        _l = self.seq_lengths[start:end]
-        if truncate:
-            _x = _x[:,:-1,:]
-            _y = _y[:,:-1,:]
         if ind_batch_X:
             _x = self.inp_inds[start:end].astype(int)
-            if truncate: _x = _x[:,:-1]
         if ind_batch_Y:
             _y = self.out_inds[start:end].astype(int)
-            if truncate: _y = _y[:,:-1]
-        return _x, _y, _l
-
-    def get_oh(self, s):
-        return self.oh_map[s]
+        return _x, _y
 
     def all_seqs(self):
         return (self.inp_seqs, self.out_seqs)
 
+    def get_oh(self, s):
+        return self.oh_map[s]
+
+    def show_oh_map(self):
+        _map = self.oh_map
+        length = _map.pop('_length_')
+        print('vocab_size = {}'.format(length))
+        for k, v in self.oh_map.items():
+            print(k, v)
 
 def main():
-    data = DataSet('240.pkl')
+    data = DataSet('/Users/alexten/Projects/PDP/SRN/pickles/a_to_x.pkl')
     data.raw2onehot()
-    print(data.raw)
+    data.raw2inds()
 
-    for i in range(8):
-        x, y = data.next_batch(2)
+
+    if len(data.raw) > 20:
+        print('raw contains {} sequences. You shouldn\'t display so much output. Here\'s a sample of 15 items:'.format(len(data.raw)))
+        sample = random.sample(data.raw, 10)
+        for s in sample:
+            print('{0:4}  {1}'.format(data.raw.index(s), s, align='left'))
+    else:
+        for i, s in enumerate(data.raw):
+            print('{0:4}  {1}'.format(i,s))
+
+    data.show_oh_map()
+    for i in range(2):
+        x, y = data.next_batch(2, ind_batch_X=True, ind_batch_Y=True)
+        print(x)
+        print(y)
+
 
 if __name__=='__main__': main()
