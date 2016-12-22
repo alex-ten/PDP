d6fc859e8d4edd4bfb8b52b94311fc692cc6e360
alexten <tenalexander1991@gmail.com>
2016-12-22 13:14:10 +0100

    -

--- classes/MIA_Viewer.py
+++ classes/MIA_Viewer.py
@@ -5,8 +5,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk
 from tkinter import ttk
 import matplotlib.pyplot as plt
-path_to_wordsr = os.getcwd()+'/raw/wordsr.txt'
-
+path_to_wordsr = os.getcwd()+'MIA/raw/wordsr.txt'
 
 wordlabs = []
 with open(path_to_wordsr, 'r') as words_file:
