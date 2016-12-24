318685d38d589ab4c359b426b7962f8be897f978
alexten <tenalexander1991@gmail.com>
2016-12-24 03:23:11 +0100

    clean up

--- MIA/Bayes_examples/bayes_simple_example.py
+++ MIA/Bayes_examples/bayes_simple_example.py
@@ -1,5 +1,5 @@
 import matplotlib
-matplotlib.rcParams['backend'] = 'TkAgg'
+matplotlib.rcParams['backend'] = 'Agg'
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import style
