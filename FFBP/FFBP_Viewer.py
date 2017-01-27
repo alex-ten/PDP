9c8d374c444afbd87bc998976d5549bc2d8e9ef7
alexten <tenalexander1991@gmail.com>
2017-01-27 22:00:52 +0000

    Create viewer

--- FFBP/FFBP_Viewer.py
+++ FFBP/FFBP_Viewer.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from FFBP.visualization.VisLayersApp import VisLayersApp as VLA
+from FFBP.visualization.NetworkData import NetworkData
 import pickle
 import os
 
@@ -7,28 +8,31 @@ class ViewerExecutive():
     def __init__(self, master):
         self.master = master
         self.master.state('withdrawn')
-        self.viewers = []
-        self._intcount = 1
-    def view(self, path):
-        log_ind = os.path.splitext(path)[0].split(sep='/')[-1].split(sep='_')[-1]
-        new_window = tk.Toplevel(self.master)
-        _figinds = [self._intcount, 100+self._intcount]
-        self.viewers.append(VLA(new_window, path, 'FFBPlog_{}'.format(log_ind), _figinds))
-        self._intcount += 1
+        self.viewers = {}
+    def view(self, snap, ID):
+        if ID not in self.viewers.keys():
+            new_window = tk.Toplevel(self.master)
+            self.viewers[ID] = VLA(new_window, snap, 30, 96)
+        else:
+            self.viewers[ID].catch_up(snap)
 
 
 def main():
     proceed = True
     VEX = ViewerExecutive(tk.Tk())
     while proceed:
-        path = input('[FFBP Viewer] Enter name of log directory OR corresponding index: ')
+        path = input('[FFBP Viewer] Enter name of log directory OR corresponding index: ')\
+        # Get path to snap file
         try:
-            int(path)
-            path = 'FFBP/logs/FFBPlog_{}/snap.pkl'.format(path)
+            ID = int(path)
+            path = os.getcwd()+'/FFBP/logs/FFBPlog_{}/snap.pkl'.format(path)
         except ValueError:
-            path = 'FFBP/logs/' + path
-        with open(path) as snap:
-            VEX.view(pickle.opensnap)
+            ID = int(path.split(sep='_')[-1])
+            path = os.getcwd()+'/FFBP/logs/' + path + '/snap.pkl'
+        with open(path, 'rb'):
+            snap = NetworkData(path)
+            VEX.view(snap, ID)
+
         print('[FFBP Viewer] Would you like to proceed?')
         prompt = input("[y/n] -> ")
         if prompt == 'n': proceed = False
