78edca0158b128d965a1772a822f63ca82c79400
alexten <tenalexander1991@gmail.com>
2017-02-08 21:19:11 +0000

    Add new functions
    Also add a reference wikipedia article

--- utilities/activation_functions.py
+++ utilities/activation_functions.py
@@ -1,4 +1,7 @@
+# Consult wikipedia for overview of these functions:
+# https://en.wikipedia.org/wiki/Activation_function
 import tensorflow as tf
+
 
 def linear(x, name='linear'):
     return tf.identity(x, name=name)
@@ -12,5 +15,11 @@ def tanh(x, name='tanh'):
 def softmax(x, name='softmax'):
     return tf.nn.softmax(x, name=name)
 
+def softplus(x, name='softplus'):
+    return tf.nn.softplus(x, name=name)
+
 def relu(x, name='relu'):
     return tf.nn.relu(x, name=name)
+
+def softsign(x, name='softsign'):
+    return tf.nn.softsign(x, name=name)
