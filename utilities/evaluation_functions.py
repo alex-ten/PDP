b2f196c9653a16597b0c0cc309944d5ffac0fc1a
alexten <tenalexander1991@gmail.com>
2017-02-08 21:19:48 +0000

    fix the

--- utilities/evaluation_functions.py
+++ utilities/evaluation_functions.py
@@ -6,8 +6,9 @@ def clipped(x):
     return tf.clip_by_value(x, 1e-10, 1.0)
 
 def tce(target, activation):
-    return tf.reduce_sum(target * tf.log(clipped(activation)) + (1 - target) * tf.log(clipped(1 - activation)),
+    return -tf.reduce_sum(target * tf.log(clipped(activation)) + (1 - target) * tf.log(clipped(1 - activation)),
                           name='tce')
 
 def tss(target, activation):
-    return tf.reduce_sum(tf.squared_difference(target, activation), name='tss')
+    return tf.reduce_sum(tf.squared_difference(target, activation),
+                         name='tss')
