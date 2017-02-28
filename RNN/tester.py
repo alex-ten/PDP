6cf5b43237ae2e598ddbcb488e1f24790e10d0a5
alexten <tenalexander1991@gmail.com>
2017-02-28 16:43:42 +0000

    Add run_test function

--- RNN/tester.py
+++ RNN/tester.py
@@ -24,6 +24,8 @@ flags.DEFINE_string("vocab", None, "Path to vocabulary")
 
 FLAGS = flags.FLAGS
 
+def peek(a):
+    print(np.around(a,2))
 
 def data_type():
   return tf.float32
@@ -36,17 +38,37 @@ def load_configs(path):
 
 
 def run_test(session, model, model_input):
+    np.set_printoptions(2,suppress=True)
+
+    # Run test epoch
     perp, preds = run_epoch(session, model)
+
+    # Fetch useful information about the test
+    targs = model_input.targets.reshape([1,-1])
     meta = model_input.meta
-    print(perp)
-    print(preds)
-    print(meta)
-    return preds
+    num_steps = model_input.num_steps
+    batch_size = model_input.batch_size
+    num_condits = len(meta)
+    num_preds = num_steps * batch_size
+    max_targs = max([x[1] for x in meta])
+
+    # Create an empty container
+    r = np.empty([num_condits, max_targs])
+    r[:] = np.NAN
+
+    # Extract useful data
+    targ_preds = np.squeeze(preds[np.arange(num_preds),targs])
+    j = 0
+    for i, (c, ppc) in enumerate(meta):
+        crit_inds = [x for x in range(c-1,num_steps*ppc-1,num_steps)]
+        condit = targ_preds[j:j + num_steps * ppc]
+        r[i,0:ppc] = condit[crit_inds]
+        j += num_steps * ppc
+    return r
 
 def main(_):
     vocab = reader.get_vocab(FLAGS.vocab)
     test_ids, test_meta = reader.make_test(PDPATH('/RNN/test_data/'+FLAGS.test), vocab)
-    print(test_ids)
     model_path = PDPATH('/RNN/trained_models/') + FLAGS.model
     config = load_configs(model_path)
 
@@ -71,8 +93,6 @@ def main(_):
                     continue
 
             b = run_test(session=session, model=mtest, model_input=test_input)
-            np.set_printoptions(2,suppress=False)
-            print(np.around(b,2))
-            print(np.shape(b))
+            print(b)
 
 if __name__ == "__main__": tf.app.run()
